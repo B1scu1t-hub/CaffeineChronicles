32aa6e54def480bc88177c7f26d37565930ff7c0
Nehemiah <njgdnb@gmail.com>
2024-01-06 13:37:52 -0700

    Set up test endpoints verify functionality

--- routes/homepage.js
+++ routes/homepage.js
@@ -5,15 +5,15 @@ const router = express.Router();
 
 // Define a route
 router.get('/', (req, res) => {
-    res.send('this is user route');// this gets executed when user visit http://localhost:3000/user
+    res.send('this is homepage route');// this gets executed when user visit http://localhost:3000/homepage
 });
 
 router.get('/101', (req, res) => {
-    res.send('this is user 101 route');// this gets executed when user visit http://localhost:3000/user/101
+    res.send('this is homepage 101 route');// this gets executed when user visit http://localhost:3000/homepage/101
 });
 
 router.get('/102', (req, res) => {
-    res.send('this is user 102 route');// this gets executed when user visit http://localhost:3000/user/102
+    res.send('this is homepage 102 route');// this gets executed when user visit http://localhost:3000/homepage/102
 });
 
 // export the router module so that server.js file can use it
--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1> Hello, Express.js Server! </h1>");
+    res.send("<h1> Hello, Express.js Server! You have reached the root of things currently.... </h1>");
 });
+
+const homepageRoutes = require('./routes/homepage');
+const othertest = require('./routes/othertest');
+
+app.use('/homepage', homepageRoutes);
+app.use('/othertest', othertest);
+
 
 const port = process.env.PORT || 3000;
 
